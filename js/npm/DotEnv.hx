96a099fa298641e09693dee5e25a2c7befa5ca16
Andreas <ciscoheat@gmail.com>
2016-08-10 16:15:07 +0200

    Using DynamicAccess for parse.

--- js/npm/DotEnv.hx
+++ js/npm/DotEnv.hx
@@ -2,6 +2,7 @@ package js.npm;
 
 import js.node.Buffer;
 import js.support.Either;
+import haxe.DynamicAccess;
 
 typedef DotEnvOptions = {
 	?silent: Bool,
@@ -13,10 +14,10 @@ extern class DotEnv
 implements npm.Package.Require<"dotenv", "^2.0.0">
 {
 	@:overload(function() : Dynamic {})	
-	public static function config(options : DotEnvOptions) : Dynamic;
+	public static function config(options : DotEnvOptions) : Void;
 
 	@:overload(function() : Dynamic {})	
-	public static function load(options : DotEnvOptions) : Dynamic;
+	public static function load(options : DotEnvOptions) : Void;
 
-	public static function parse(buf : Either<String, Buffer>) : Dynamic;
+	public static function parse(buf : Either<String, Buffer>) : DynamicAccess<String>;
 }
